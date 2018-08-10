15c7f6ee1cd87576a7c6cafcffe392b8692b1855
Ketu68 <41552633+Ketu68@users.noreply.github.com>
2018-08-10 12:50:06 -0500

    First overloading

---
+++ Overloading1/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading1
+{
+    public class Person
+    {
+        public string name;
+        public List<string> Names { get; set; }
+        public List<string> IDS { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            string firstName = "Sample", lastName = "Student";
+            Console.WriteLine("Full name is: " + firstName + " " + lastName);
+        }
+    }
+
+    public class Employee : Person
+    {
+        int ID;
+
+        public static bool operator== (Employee iD1, Employee iD2)
+        {
+            bool status = false;
+            if (iD1 == iD2)
+            {
+
+                status = true;
+            }
+            return status;
+        }
+        public static bool operator!= (Employee iD1, Employee iD2)
+        {
+            bool status = false;
+            if (iD1 != iD2)
+            {
+
+                status = false;
+            }
+            return status;
+        }
+    }
+}
--- Overloading1/Program.cs
+++ Overloading1/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Overloading1
 {
-    class Program
+
+// 1. Overload the "==" operator so it checks if two Employee objects are equal by comparing their Id property.
+
+    static class Program
     {
         static void Main(string[] args)
         {
+            Person person = new Person();
+            Employee employee = new Employee();
+            employee.Names = new List<Names>();                   // <----------- Weird error here about system.collections
+
+            employee.Names.Add("Joe Schmoe");
+            employee.Names.Add("Pete Schmoe");
+            employee.Names.Add("Rachel Schmoe");
+            employee.Names.Add("Turkey Schmoe");
+
+            employee.IDS.Add("1");
+            employee.IDS.Add("2");
+            employee.IDS.Add("3");
+            employee.IDS.Add("1");
+
+            // INSERT CODE TO COMPARE ID's HERE
+            //
+            // .....
+            //
+            //
+
+            employee.SayName();
+
+            
+
+            Console.ReadLine();
         }
     }
 }
